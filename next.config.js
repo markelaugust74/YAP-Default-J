6d54a778742df92e783622ab4f8be138ee3bdac6
Markel August <146657076+markelaugust74@users.noreply.github.com>
2024-12-01 15:19:43 -0500

    Update configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'static',
+  distDir: 'dist',
+  assetPrefix: '/YAP-Default-J/',
   basePath: '/YAP-Default-J',
   images: {
     unoptimized: true
-  }
+  },
+  trailingSlash: true
 }
 
 module.exports = nextConfig
